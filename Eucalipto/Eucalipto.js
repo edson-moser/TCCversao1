d083e729c5d2af21addd55e8c83b3e1955188f09
miguelFM333 <miguelfmarchi@gmail.com>
2025-04-05 13:21:03 -0300

    fazendo 2

--- Eucalipto/Eucalipto.js
+++ Eucalipto/Eucalipto.js
@@ -7,25 +7,8 @@ function changeType(inputId, selectId) {
     input.type = selectedType;
   }
 
-  
-  const ctx = document.getElementById('barChart').getContext('2d');
-        const barChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
-                datasets: [{
-                    label: 'Vendas',
-                    data: [12, 19, 3, 5, 2],
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
+  var ctx = document.getElementsByClassName("line-chart")
+
+  var chartGrafico = new Chart(ctx, {
+    type: 'line'
+  })
